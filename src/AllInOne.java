cacb79bc5af459a2f55867e4dd6b35c042fd6427
mikulass <mikulas.strelecky@gmail.com>
2018-01-20 17:16:51 +0100

    Initial commit

---
+++ src/AllInOne.java
@@ -0,0 +1,24 @@
+
+public class AllInOne {
+
+	public static void main(String[] args) {
+
+		int x = 100;
+		
+		String y = Integer.toString(x);
+		
+		Integer b  = new Integer(x);
+		
+		int z = b.intValue();
+		
+		Integer c = new Integer(z);
+		
+		String d = c.toString(c);
+		
+		int e = Integer.parseInt(d);
+
+		
+		
+	}
+
+}
---
+++ src/Constructors.java
@@ -0,0 +1,20 @@
+
+public class Constructors {
+
+	public static void main(String[] args) {
+
+		long l = 1000;
+		
+		Long a = new Long(l);
+		
+		int i = 768;
+		
+		Integer n = new Integer(i);
+		
+		String s = "2000";
+		
+		Long b = new Long(s);
+		
+	}
+
+}
